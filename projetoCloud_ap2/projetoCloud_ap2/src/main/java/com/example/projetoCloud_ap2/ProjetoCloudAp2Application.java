66b6d7a91069ad29410a5b1bea0f0b79289731d0
Guilherme Bastos <126486967+guilhermebastos2002@users.noreply.github.com>
2024-11-19 15:05:49 -0300

    Add files via upload

---
+++ projetoCloud_ap2/projetoCloud_ap2/src/main/java/com/example/projetoCloud_ap2/ProjetoCloudAp2Application.java
@@ -0,0 +1,13 @@
+package com.example.projetoCloud_ap2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCloudAp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCloudAp2Application.class, args);
+	}
+
+}
---
+++ projetoCloud_ap2/projetoCloud_ap2/src/test/java/com/example/projetoCloud_ap2/ProjetoCloudAp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetoCloud_ap2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCloudAp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
